0608a0fe71b0bb68876a20913ea6a611e2b4b028
Jan Silva <janfrs3@gmail.com>
2022-11-26 16:11:50 +0100

    Add style for user profile box on Home
    Broken down some of the style of the app

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,27 @@
+import './Home.css';
+import ShareIcon from './share.svg';
 
 const Home = () => {
     return (
-        <h1> Home </h1>
+        <div className="app">
+            <h1>MovieLand</h1>
+        <div className="profile">
+            <div className="avatar">
+                <div className="avatar-img">
+                    <img src="https://i.pravatar.cc/300"/>
+                </div>
+                <p>Jan Silva</p>
+            </div>
+            <div className="description">
+                Passionate about horror movies born in Brazil
+            </div>
+            <div className="share">
+                <img src={ShareIcon} alt="Share your Profile"/>
+            </div>
+        </div>
+            <div className="container">
+            </div>
+        </div>
     );
 }
 
