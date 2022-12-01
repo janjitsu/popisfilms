e9ea176799b498bf9c28ab23a3aff66c00d6d16d
Jan Silva <janfrs3@gmail.com>
2022-12-01 18:47:05 +0100

    Improve user profile
    Add check for used logged in

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,37 +1,93 @@
 import './Home.css';
 import ShareIcon from './share.svg';
+import AddIcon from './add.svg';
+import LogoutIcon from './logout.svg';
+import MovieCard from 'components/MovieCard/MovieCard.jsx';
 /** providers */
 import { useSession } from "providers/Session";
+import {useState, useEffect} from 'react';
 
 const Home = () => {
-    const { user, loginMethod } = useSession();
+    const { user, loginMethod, logoutMethod } = useSession();
+    const [state, setState] = useState({
+        loading: true,
+    });
+    const favorites = [
+        {
+            "Title": "Belle de Jour",
+            "Year": "1967",
+            "imdbID": "tt0061395",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BZjNkNGJjYWEtM2IyNi00ZjM5LWFlYjYtYjQ4NTU5MGFlMTI2XkEyXkFqcGdeQXVyMTMxMTY0OTQ@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Belle De Jour Remake",
+            "Year": "2017",
+            "imdbID": "tt6553072",
+            "Type": "movie",
+            "Poster": "N/A"
+        }
+    ]
+
+    useEffect(() => {
+      setTimeout(()=>{
+          setState({ ...state, loading: false});
+      }, 1000)
+    }, [user]);
+
 
     return (
         <div className="app">
             <h1>MovieLand</h1>
-            {user?.displayName ? (
-                <div className="profile">
-                    <div className="avatar">
-                        <div className="avatar-img">
-                            <img src={user.photoURL} alt={user.displayName}/>
-                        </div>
-                        <p>{user.displayName}</p>
-                    </div>
-                    <div className="description">
-                        Passionate about horror movies born in Brazil
-                    </div>
-                    <div className="share">
-                        <img src={ShareIcon} alt="Share your Profile"/>
-                    </div>
-                </div>
+            {state.loading ? (
+                <div className="loading">⌛Loading...</div>
             ) : (
                 <>
-                <div className="description">
-                    Login to save your favorite Movies
-                </div>
-                    <button type="button" class="login-btn" onClick={() => loginMethod()}>
-                    Sign in with Google
-                </button>
+                {user?.displayName ? (
+                    <>
+                        <div className="profile">
+                            <div className="avatar">
+                                <div className="avatar-img">
+                                    <img src={user.photoURL} alt={user.displayName}/>
+                                </div>
+                                <p>{user.displayName}</p>
+                            </div>
+                            <div className="side-box">
+                                <div className="description">
+                                    Passionate about horror movies born in Brazil
+                                </div>
+                                <div className="actions">
+                                    <input type="image" src={AddIcon} alt="Add Favorite Movies" title="Add Favorite Movies"/>
+                                    <input type="image" src={ShareIcon} alt="Share your Profile" title="Share your Profile"/>
+                                    <input type="image" src={LogoutIcon} alt="Logout" title="Logout" onClick={()=>logoutMethod()}/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="favorites">
+                            <h1>{user.displayName}'s Favorite Movies</h1>
+                            {favorites?.length > 0 ? (
+                                <div className="container">
+                                    {favorites.map((movie) => (
+                                        <MovieCard movie={movie} key={movie.imdbID}/>
+                                    ))}
+                                </div>
+                            ) : (
+                                <div className="empty">
+                                    <h2>No favorites found :(</h2>
+                                </div>
+                            )}
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <div className="description">
+                            Login to save your favorite Movies
+                        </div>
+                        <button type="button" className="login-btn" onClick={() => loginMethod()}>
+                            Sign in with Google
+                        </button>
+                    </>
+                )}
                 </>
             )}
         </div>
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,8 +1,20 @@
+import { useEffect, useContext } from "react";
+
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Search from 'pages/Search/Search.js';
 import Home from 'pages/Home/Home.js';
+import { SessionContext } from "providers/Session";
 
 const AppRoutes = () => {
+    const { user, checkUserLogin } = useContext(SessionContext);
+    useEffect(() => {
+        isValid();
+    }, []);
+
+    const isValid = async () => {
+        await checkUserLogin();
+    };
+
     return (
     <BrowserRouter>
         <Routes>
