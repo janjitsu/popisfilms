3fe31d45bebc3190e8da70c366a20ac57a160839
Jan Silva <janfrs3@gmail.com>
2022-12-05 15:13:49 +0100

    Trigger SearchBar when pressing enter

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,12 +3,18 @@ import SearchIcon from './search.svg';
 import './SearchBar.css';
 
 const SearchBar = (props, {searchTerm, handleOnChange, handleOnClick}) => {
+    const handleHitEnter = (e) => {
+        if (e.key === 'Enter') {
+            props.handleOnClick()
+        }
+    }
     return (
             <div className="search">
                 <input
                     placeholder="Search for movies"
                     value={ props.searchTerm }
                     onChange={(e) => props.handleOnChange(e.target.value)}
+                    onKeyDown={(e) => handleHitEnter(e)}
                 />
                 <img
                     src={ SearchIcon }
