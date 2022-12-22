50f992df2aeced99027b693a1c7b65b91d04781c
Jan Silva <janfrs3@gmail.com>
2022-12-22 14:37:18 +0100

    [frontend] Add user token on backend request

--- frontend/src/services/backend.js
+++ frontend/src/services/backend.js
@@ -1,10 +1,22 @@
 const BACKEND_URL = `http://localhost:8080`;
 
+const authHeaders = (user) =>
+    new Promise( async (resolve) => {
+        const userToken = await user.getIdToken()
+        resolve(
+          {
+            headers: {
+              'Authorizaton': 'Bearer ' + userToken
+            }
+          }
+        )
+    });
+
 const getFavorites = (user) =>
     new Promise( async (resolve) => {
-        const response = await fetch(`${BACKEND_URL}/favorites`)
+        const authHeader = await authHeaders(user)
+        const response = await fetch(`${BACKEND_URL}/favorites`, authHeader)
         const data = await response.json();
-
         resolve(data);
     });
 
