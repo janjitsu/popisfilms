56887adda5b470eb4b6b8484168e707f837bc087
glauroqj <glauro.juliani@hotmail.com>
2022-12-27 18:23:51 -0300

    fix

--- frontend/src/routes/AppRoutes.js
+++ frontend/src/routes/AppRoutes.js
@@ -1,30 +1,30 @@
 import { useEffect, useContext, memo } from "react";
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Search from 'pages/Search/Search.js';
-import Home from 'pages/Home/Home.js';
+import Search from "pages/Search/Search.js";
+import Home from "pages/Home/Home.js";
 import { SessionContext } from "providers/Session";
 
 const AppRoutes = () => {
-    const { checkUserLogin } = useContext(SessionContext);
+  const { checkUserLogin } = useContext(SessionContext);
 
-    useEffect(() => {
-      const isValid = async () => {
-          await checkUserLogin();
-      };
-      isValid();
-    }, [checkUserLogin]);
+  useEffect(() => {
+    isValid();
+  }, []);
 
-    return (
+  const isValid = async () => {
+    await checkUserLogin();
+  };
+
+  return (
     <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/add" element={<Search />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/add" element={<Search />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </BrowserRouter>
-
-    );
-}
+  );
+};
 
 export default memo(AppRoutes);
