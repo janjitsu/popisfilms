ad043439c2cdfe67bc7762c12e4342f96c01bb43
Jan Silva <janfrs3@gmail.com>
2022-12-30 01:07:31 +0100

    [frontend] Move code to subfolder webapp

--- frontend/src/webapp/index.js
+++ frontend/src/webapp/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/style/index.css';
-import reportWebVitals from './reportWebVitals';
 import AppRoutes from './routes/AppRoutes.js';
 /** firebase */
 import credentials from "./credentials";
 import firebase from "firebase/compat/app";
 /** provider */
-import { SessionProvider } from "providers/Session";
+import { SessionProvider } from "./providers/Session";
 
 /** start */
 firebase.initializeApp(credentials);
@@ -20,8 +19,3 @@ root.render(
       </SessionProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
